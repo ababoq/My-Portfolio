d10d73db4e44db0536ca04d7605547134b9160cb
Wassim <wbenguerine@gmail.com>
2023-10-15 23:32:14 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,365 @@
+//Header fixed
+const projects = [{
+        title: "",
+        description: "",
+        images: [
+            { type: "image", src: "./img/portfolio/Bettor/iamge.png", title: "" },
+            { type: "image", src: "./img/portfolio/bettor/bet1.png", title: "" },
+            { type: "image", src: "./img/portfolio/bettor/bet2.png", title: "" },
+            { type: "image", src: "./img/portfolio/bettor/bet3.png", title: "" },
+            { type: "image", src: "./img/portfolio/bettor/bet4.png", title: "" },
+            { type: "image", src: "./img/portfolio/bettor/bet5.png", title: "" },
+            { type: "image", src: "./img/portfolio/bettor/bet6.png", title: "" },
+            { type: "image", src: "./img/portfolio/bettor/bet7.png", title: "" },
+            { type: "image", src: "./img/portfolio/bettor/bet8.png", title: "" },
+            { type: "video", src: "./img/portfolio/bettor/demo.mp4", title: "" }
+
+        ],
+        additionalText: "",
+    },
+    {
+        title: "",
+        description: "",
+        images: [
+            { type: "image", src: "./img/portfolio/health/health.png", title: "" },
+            { type: "image", src: "./img/portfolio/health/image.png", title: "" },
+            { type: "image", src: "./img/portfolio/health/image2.png", title: "" },
+            { type: "image", src: "./img/portfolio/health/image3.png", title: "" },
+            { type: "image", src: "./img/portfolio/health/image4.png", title: "" },
+            { type: "image", src: "./img/portfolio/health/image5.png", title: "" },
+            { type: "image", src: "./img/portfolio/health/image6.png", title: "" },
+            { type: "video", src: "./img/portfolio/health/demo.mp4", title: "" },
+
+
+        ],
+        additionalText: ""
+    },
+    {
+        title: "",
+        description: "",
+        images: [
+            { type: "image", src: "./img/portfolio/estm/login.png", title: "" },
+            { type: "image", src: "./img/portfolio/estm/admin absent.png", title: "" },
+            { type: "image", src: "./img/portfolio/estm/admin conge.png", title: "" },
+            { type: "image", src: "./img/portfolio/estm/admin employer.png", title: "" },
+            { type: "image", src: "./img/portfolio/estm/admin news.png", title: "" },
+            { type: "image", src: "./img/portfolio/estm/admin sanction.png", title: "" },
+            { type: "image", src: "./img/portfolio/estm/admin settings.png", title: "" },
+            { type: "image", src: "./img/portfolio/estm/dark mode user.png", title: "" },
+            { type: "image", src: "./img/portfolio/estm/dark mode.png", title: "" },
+            { type: "image", src: "./img/portfolio/estm/user actu.png", title: "" },
+            { type: "image", src: "./img/portfolio/estm/user infos.png", title: "" },
+            { type: "video", src: "./img/portfolio/estm/demo.mp4", title: "" },
+
+
+        ],
+        additionalText: ""
+    },
+
+    {
+        title: "",
+        description: "",
+        images: [
+            { type: "image", src: "./img/portfolio/ocr/extract.png", title: "" },
+            { type: "image", src: "./img/portfolio/ocr/imageweb.png", title: "" },
+            { type: "image", src: "./img/portfolio/ocr/imageupload.png", title: "" },
+            { type: "image", src: "./img/portfolio/ocr/imageprocess.png", title: "" },
+            { type: "image", src: "./img/portfolio/ocr/imageoutput.png", title: "" },
+            { type: "image", src: "./img/portfolio/ocr/image.png", title: "" },
+            { type: "image", src: "./img/portfolio/ocr/image2.png", title: "" },
+            { type: "image", src: "./img/portfolio/ocr/image3.png", title: "" },
+            { type: "image", src: "./img/portfolio/ocr/image4.png", title: "" },
+            { type: "image", src: "./img/portfolio/ocr/image5.png", title: "" },
+            { type: "image", src: "./img/portfolio/ocr/image6.png", title: "" },
+            { type: "video", src: "./img/portfolio/ocr/demo.mp4", title: "" },
+
+
+
+        ],
+        additionalText: ""
+    },
+
+
+    {
+        title: "",
+        description: "",
+        images: [
+            { type: "image", src: "./img/portfolio/map/image.png", title: "" },
+            { type: "image", src: "./img/portfolio/map/image2.png", title: "" },
+            { type: "image", src: "./img/portfolio/map/image3.png", title: "" },
+            { type: "image", src: "./img/portfolio/map/image4.png", title: "" },
+
+
+        ],
+        additionalText: ""
+    },
+
+    {
+        title: "",
+        description: "",
+        images: [
+            { type: "image", src: "./img/portfolio/graphy/image.png", title: "" },
+            { type: "image", src: "./img/portfolio/graphy/image2.png", title: "" },
+            { type: "image", src: "./img/portfolio/graphy/image3.png", title: "" },
+            { type: "image", src: "./img/portfolio/graphy/image4.png", title: "" },
+            { type: "image", src: "./img/portfolio/graphy/image5.png", title: "" },
+            { type: "image", src: "./img/portfolio/graphy/image6.png", title: "" },
+            { type: "video", src: "./img/portfolio/graphy/demo.mp4", title: "" },
+
+
+        ],
+        additionalText: ""
+    },
+
+    {
+        title: "",
+        description: "",
+        images: [
+            { type: "image", src: "./img/portfolio/lamp/image.png", title: "" },
+            { type: "image", src: "./img/portfolio/lamp/image1.png", title: "" },
+            { type: "image", src: "./img/portfolio/lamp/image2.png", title: "" },
+            { type: "image", src: "./img/portfolio/lamp/image3.png", title: "" },
+            { type: "image", src: "./img/portfolio/lamp/image4.png", title: "" },
+            { type: "video", src: "./img/portfolio/lamp/demo.mp4", title: "" },
+
+
+        ],
+        additionalText: ""
+    },
+
+    {
+        title: "",
+        description: "",
+        images: [
+            { type: "image", src: "./img/portfolio/sofascrap/image.png", title: "" },
+            { type: "image", src: "./img/portfolio/sofascrap/image1.png", title: "" },
+            { type: "image", src: "./img/portfolio/sofascrap/image2.png", title: "" },
+            { type: "image", src: "./img/portfolio/sofascrap/image3.png", title: "" },
+            { type: "image", src: "./img/portfolio/sofascrap/image4.png", title: "" },
+            { type: "video", src: "./img/portfolio/sofascrap/demo.mp4", title: "" },
+
+
+        ],
+        additionalText: ""
+    },
+
+    {
+        title: "",
+        description: "",
+        images: [
+            { type: "image", src: "./img/portfolio/telegram/image.png", title: "" },
+            { type: "image", src: "./img/portfolio/telegram/image1.png", title: "" },
+            { type: "image", src: "./img/portfolio/telegram/image2.png", title: "" },
+            { type: "image", src: "./img/portfolio/telegram/image3.png", title: "" },
+            { type: "image", src: "./img/portfolio/telegram/image4.png", title: "" },
+            { type: "image", src: "./img/portfolio/telegram/image5.png", title: "" },
+            { type: "image", src: "./img/portfolio/telegram/image6.png", title: "" },
+            { type: "image", src: "./img/portfolio/telegram/image7.png", title: "" },
+
+            { type: "video", src: "./img/portfolio/telegram/demo.mp4", title: "" },
+
+
+        ],
+        additionalText: ""
+    },
+];
+
+window.onscroll = function() {
+    const docScrollTop = document.documentElement.scrollTop;
+
+    if (window.innerWidth > 991) {
+        if (docScrollTop > 100) {
+            document.querySelector("header").classList.add("fixed");
+        } else {
+            document.querySelector("header").classList.remove("fixed");
+        }
+    }
+}
+
+
+
+
+//navbar links
+
+const navbar = document.querySelector(".navbar");
+a = navbar.querySelectorAll("a")
+
+a.forEach(function(element) {
+    element.addEventListener("click", function() {
+        for (let i = 0; i < a.length; i++) {
+            a[i].classList.remove("active")
+        }
+        this.classList.add("active");
+        document.querySelector(".navbar").classList.toggle("show");
+    })
+})
+
+
+//Hamburger
+
+const hamBurger = document.querySelector(".hamburger");
+
+hamBurger.addEventListener("click", function() {
+    document.querySelector(".navbar").classList.toggle("show");
+})
+
+//Portfolio Gallery
+const galleryItem = document.querySelectorAll(".item"); // Define it here
+
+const lightbox = document.querySelector(".lightbox");
+const lightboxImg = lightbox.querySelector("img");
+galleryItem.forEach(function(element, index) {
+    element.querySelector(".fa-plus").addEventListener("click", function() {
+        let project = projects[index]; // Get the project from the array
+
+        let mediaContainer = document.getElementById("imageContainer");
+        mediaContainer.innerHTML = ""; // Clear previous media
+
+        // Loop through media items and append them to the container
+        for (let media of project.images) {
+            if (media.type === "image" || !media.type) { // If type is image or not provided
+                let img = document.createElement("img");
+                img.src = media.src;
+                img.alt = project.title;
+                img.style.width = "100%";
+                img.style.maxWidth = "900px";
+                mediaContainer.appendChild(img);
+
+                // Create and append the image title
+                let imgTitle = document.createElement("p");
+                imgTitle.textContent = media.title;
+                imgTitle.style.textAlign = "center"; // Center the title below the image
+                mediaContainer.appendChild(imgTitle);
+            } else if (media.type === "video") { // If type is video
+                let video = document.createElement("video");
+                video.src = media.src;
+                video.controls = true; // Add video controls
+                video.style.width = "100%";
+                video.style.maxWidth = "900px";
+                mediaContainer.appendChild(video);
+
+                // Create and append the video title
+                let videoTitle = document.createElement("p");
+                videoTitle.textContent = media.title;
+                videoTitle.style.textAlign = "center"; // Center the title below the video
+                mediaContainer.appendChild(videoTitle);
+            }
+        }
+
+        document.getElementById("projectInfo").textContent = project.description;
+        document.getElementById("additionalText").textContent = project.additionalText;
+
+        document.getElementById("projectModal").style.display = "flex";
+    });
+});
+
+
+
+
+let modal = document.getElementById("projectModal");
+let closeBtn = modal.querySelector(".close");
+
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+// Testimonials Slider
+
+const sliderContainer = document.querySelector(".testimonials-box");
+const slider = sliderContainer.children;
+const containerWidth = sliderContainer.offsetWidth;
+
+const margin = 30;
+let itemPerSlide = 0;
+let sliderDot;
+
+const responsive = [{
+        breakPoint: {
+            width: 0,
+            item: 1,
+        },
+    },
+    {
+        breakPoint: {
+            width: 991,
+            item: 2,
+        },
+    },
+];
+
+function load() {
+    for (let i = 0; i < responsive.length; i++) {
+        if (window.innerWidth > responsive[i].breakPoint.width) {
+            itemPerSlide = responsive[i].breakPoint.item;
+        }
+    }
+    start();
+}
+
+function start() {
+    totalWidth = 0;
+    for (let i = 0; i < slider.length; i++) {
+        slider[i].style.width = containerWidth / itemPerSlide - margin + "px";
+        slider[i].style.margin = margin / 2 + "px";
+        totalWidth += containerWidth / itemPerSlide;
+    }
+    sliderContainer.style.width = totalWidth + "px";
+
+    sliderDot = Math.ceil(slider.length / itemPerSlide);
+
+    for (let i = 0; i < sliderDot; i++) {
+        const div = document.createElement("div");
+        div.id = i;
+        div.setAttribute("onclick", "controlSlide(this)");
+        if (i == 0) {
+            div.classList.add("active");
+        }
+        document.querySelector(".slider").appendChild(div);
+    }
+}
+
+let currentSlide = 0;
+let autoSlide = 0;
+
+function controlSlide(element) {
+    clearInterval(timer);
+    timer = setInterval(autoPlay, 5000);
+    autoSlide = element.id;
+    currentSlide = element.id;
+    changeSlide(currentSlide);
+}
+
+function changeSlide(currentSlide) {
+    controlButton = document.querySelector(".slider").children;
+    for (let i = 0; i < controlButton.length; i++) {
+        controlButton[i].classList.remove("active");
+    }
+    controlButton[currentSlide].classList.add("active");
+
+    sliderContainer.style.marginLeft = -(containerWidth * currentSlide) + "px";
+}
+
+function autoPlay() {
+    if (autoSlide == sliderDot - 1) {
+        autoSlide = 0;
+    } else {
+        autoSlide++;
+    }
+    changeSlide(autoSlide);
+}
+let timer = setInterval(autoPlay, 5000);
+
+window.onload = load();
+
+
+//Footer Year 
+var year = document.getElementById("year");
+year.innerHTML = new Date().getFullYear();
